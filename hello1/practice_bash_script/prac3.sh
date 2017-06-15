320721980d22208044a301fe409b0ba828f6616e
thuongnguyen <nththuong2606@gmail.com>
2017-06-15 18:07:41 +0700

    practice bash script

---
+++ hello1/practice_bash_script/prac3.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "A:"
+read A
+echo "B:"
+read B
+echo "C:"
+read C
+if [[ $A -eq 0 ]]; 
+	elif [[ $B -eq 0 ]]; 
+		elif [[ $C -eq 0 ]]; then
+			echo "Equation invalid"
+		then
+			echo "Equation has no experimental"
+	then
+		echo "Equation has 1 experimental"
+else
+	echo "Equation has 2 experimental"
+
+fi
---
+++ hello1/practice_bash_script/practice3.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+echo "A:"
+read A
+echo "B:"
+read B
+echo "C:"
+read C
+
+echo A,B,C
